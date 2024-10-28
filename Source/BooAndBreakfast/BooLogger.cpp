75d9b4fb3b4da6652bab4b4e592fc639b2e677aa
ASFASFHLK <112550984+ASFASFHLK@users.noreply.github.com>
2024-10-28 17:35:14 +0100

    testing readWrite saken

---
+++ Source/BooAndBreakfast/BooLogger.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BooLogger.h"
+
+// Sets default values
+ABooLogger::ABooLogger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABooLogger::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABooLogger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABooLogger::Log()
+{
+	FString File = FPaths::ProjectConfigDir();
+	File.Append(TEXT("ActionLog.txt"));
+	IPlatformFile& FileManager = FPlatformFileManager::Get().GetPlatformFile();
+	FString FileContent;
+	if(FileManager.FileExists(*File))
+	{
+		if(FFileHelper::LoadFileToString(FileContent, *File, FFileHelper::EHashOptions::None))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("FileManipulation: Text from File: %s"), *FileContent);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to load text from file"));
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("File not loaded at %s"), *File);
+	}
+}
+
---
+++ Source/BooAndBreakfast/BooLogger.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BooLogger.generated.h"
+
+UCLASS()
+class BOOANDBREAKFAST_API ABooLogger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABooLogger();
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void Log();
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+};
