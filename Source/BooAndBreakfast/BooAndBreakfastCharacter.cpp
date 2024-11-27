945c54ddde1bac7a1873200ad169bc8e25ffca15
ASFASFHLK <112550984+ASFASFHLK@users.noreply.github.com>
2024-11-27 11:02:04 +0100

    taktile keybindings

--- Source/BooAndBreakfast/BooAndBreakfastCharacter.cpp
+++ Source/BooAndBreakfast/BooAndBreakfastCharacter.cpp
@@ -74,25 +74,25 @@ void ABooAndBreakfastCharacter::Repeat()
 	{
 		Introduction->RepeatLastInterview();
 	}
-	else
-	{
-		switch (CurrentRoom)
-		{
-		case 0:
-			CurrentRoom = 2;
-			TeleportTo(PositionsToTeleportTo[2], RotationsToTeleportTo[2]);
-			break;
-		case 1:
-			--CurrentRoom;
-			TeleportTo(PositionsToTeleportTo[0], RotationsToTeleportTo[0]);
-			break;
-		case 2:
-			--CurrentRoom;
-			TeleportTo(PositionsToTeleportTo[1], RotationsToTeleportTo[1]);
-			break;
-		default: ;
-		}
-	}
+	// else
+	// {
+	// 	switch (CurrentRoom)
+	// 	{
+	// 	case 0:
+	// 		CurrentRoom = 2;
+	// 		TeleportTo(PositionsToTeleportTo[2], RotationsToTeleportTo[2]);
+	// 		break;
+	// 	case 1:
+	// 		--CurrentRoom;
+	// 		TeleportTo(PositionsToTeleportTo[0], RotationsToTeleportTo[0]);
+	// 		break;
+	// 	case 2:
+	// 		--CurrentRoom;
+	// 		TeleportTo(PositionsToTeleportTo[1], RotationsToTeleportTo[1]);
+	// 		break;
+	// 	default: ;
+	// 	}
+	// }
 }
 void ABooAndBreakfastCharacter::Proceed()
 {
